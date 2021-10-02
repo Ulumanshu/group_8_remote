72443f59f247ae97450bb67a627931bb74a02377
leliuxLI <64489616+leliuxLI@users.noreply.github.com>
2021-10-02 12:42:32 +0300

    Ignas

--- abstract_classes.py
+++ abstract_classes.py
@@ -9,6 +9,9 @@ class Person:
     def __str__(self):
         return f"{self.name} is {self.age} years old"
 
+    def show_finance(self):
+        return 0
+
 
 # Simple inheritance
 class Employee(Person):
@@ -41,7 +44,7 @@ class WorkingStudent(Employee, Student):
         Student.__init__(self, name, age, scholarship)
 
     def show_finance(self):
-        return self.rate * self.num_of_hour + self.scholarship
+        return self.rate * self.num_of_hours + self.scholarship
 
 
 if __name__ == "__main__":
@@ -49,6 +52,9 @@ if __name__ == "__main__":
     os2 = Employee("Jack", 36, 20, 160)
     os3 = Student("Agatha", 22, 1000)
     os4 = WorkingStudent("Monica", 24, 9.5, 70, 550)
+    object_list = [os1, os2, os3, os4]
+    for i in object_list:
+        print(f'{i} finasai: {i.show_finance()}')
     print(os1)
     print(os2)
     print(os3)
