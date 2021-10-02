2eda53dab8f10bb97866f520c19c2067d09d74c3
Ulumanshu <karolis_kesminas@yahoo.com>
2021-10-02 12:25:31 +0300

    Add multiple inheritance example

--- abstract_classes.py
+++ abstract_classes.py
@@ -10,9 +10,11 @@ class Person:
         return f"{self.name} is {self.age} years old"
 
 
+# Simple inheritance
 class Employee(Person):
     def __init__(self, name, age, rate, num_of_hours):
-        super().__init__(name, age)
+        # Specify what super class to use if using multiple inheritance
+        Person.__init__(self, name, age)
         self.rate = rate
         self.num_of_hours = num_of_hours
 
@@ -20,19 +22,34 @@ class Employee(Person):
         return self.rate * self.num_of_hours
 
 
+# Simple inheritance
 class Student(Person):
     def __init__(self, name, age, scholarship):
-        super().__init__(name, age)
+        # Specify what super class to use if using multiple inheritance
+        Person.__init__(self, name, age)
         self.scholarship = scholarship
 
     def show_finance(self):
         return self.scholarship
 
 
+# Multiple inheritance
+class WorkingStudent(Employee, Student):
+    def __init__(self, name, age, rate, num_of_hour, scholarship):
+        # Specify what super class to use if using multiple inheritance
+        Employee.__init__(self, name, age, rate, num_of_hour)
+        Student.__init__(self, name, age, scholarship)
+
+    def show_finance(self):
+        return self.rate * self.num_of_hour + self.scholarship
+
+
 if __name__ == "__main__":
     os1 = Person("John", 54)
     os2 = Employee("Jack", 36, 20, 160)
     os3 = Student("Agatha", 22, 1000)
+    os4 = WorkingStudent("Monica", 24, 9.5, 70, 550)
     print(os1)
     print(os2)
     print(os3)
+    print(os4)
