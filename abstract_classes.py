70365f5a7a238b537a513d26ea9a94de15bff041
Ulumanshu <karolis_kesminas@yahoo.com>
2021-10-02 13:30:33 +0300

    Add abstract classes

--- abstract_classes.py
+++ abstract_classes.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from abc import ABC, abstractmethod
+from math import pi
+
+
 # Paprasta klase
 class Person:
     def __init__(self, name, age):
@@ -50,7 +54,46 @@ class WorkingStudent(Student, Employee):
         return self.rate * self.num_of_hours + self.scholarship
 
 
+# Abstract class example
+# Abstrakcios klases pavyzdys - naudojama kaip sablonas kitoms klases, @abstractmethod privercia kitus programuotojus
+# kuriant klases kurios paveldi Figure aprasyti area ir circuit metodus
+class Figure(ABC):
+    @abstractmethod
+    def circuit(self):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+# Class inheriting from Figure has to have circuit and area methods defined
+# Klase paveldinti is Figure turi tureti apibreztus metodus circuit ir area
+class Rectangle(Figure):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def circuit(self):
+        return 2 * (self.a + self.b)
+
+    def area(self):
+        return self.a * self.b
+
+
+class Circle(Figure):
+    def __init__(self, r):
+        self.r = r
+
+    def circuit(self):
+        return 2 * self.r * pi
+
+    def area(self):
+        return pi * self.r ** 2
+
+
 if __name__ == "__main__":
+    ##################### INHERITANVE/PAVELDEJIMAS #####################################################################
     os1 = Person("John", 54)
     os2 = Employee("Jack", 36, 20, 160)
     os3 = Student("Agatha", 22, 1000)
@@ -60,10 +103,16 @@ if __name__ == "__main__":
     # show_finance method implemented
     # Polimorfizmas, naudojamas show_finance metodas objektu sarasui, sarase objektai sukurti is skirtingu klasiu,
     # polimorfizmas ir yra tai, visos tos klases turi metoda tuo pat pavadinimu.
-    for i in object_list:
-        print(f'{i} finasai: {i.show_finance()}')
+    # for i in object_list:
+    #     print(f'{i} finasai: {i.show_finance()}')
 
     # print(os1)
     # print(os2)
     # print(os3)
     # print(os4)
+    ####################################################################################################################
+    ############### ABSTRACT CLASSES / ABSTRAKCIOS KLASES ##############################################################
+    Rectangle = Rectangle(3, 5)
+    Circle = Circle(12)
+    print(Rectangle.circuit())
+    print(Circle.circuit())
