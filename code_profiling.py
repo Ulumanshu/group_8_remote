af90154a5e38548bf2b9b5377b3f14e19fb97042
Ulumanshu <karolis_kesminas@yahoo.com>
2021-10-09 13:50:13 +0300

    Code profiling example

--- code_profiling.py
+++ code_profiling.py
@@ -24,3 +24,23 @@ def func():
 
     print(timeit.timeit(stmt=code, setup=setup))
     print(timeit.timeit(stmt=code2, setup=setup))
+
+#     setup2 = '''
+# from __main__ import linear_search, binary_search
+# import random
+#     '''
+#
+#     linear_search_code = '''
+# lst = sorted([random.randint(0, 1000000) for _ in range(1000)])
+# to_find = random.randint(0, 1000000)
+# result = linear_search(lst, to_find)
+#     '''
+#
+#     binary_search_code = '''
+# lst = sorted([random.randint(0, 1000000) for _ in range(1000)])
+# to_find = random.randint(0, 1000000)
+# result = binary_search(lst, to_find)
+#     '''
+#
+#     print(timeit.timeit(stmt=linear_search_code, setup=setup2, number=1000))
+#     print(timeit.timeit(stmt=binary_search_code, setup=setup2, number=1000))
