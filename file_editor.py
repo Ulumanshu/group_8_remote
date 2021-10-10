5a5add8392d1ba6e28865d3f561f60793f7a72ee
Ulumanshu <karolis_kesminas@yahoo.com>
2021-10-10 11:29:03 +0300

    Add task file_editor

--- file_editor.py
+++ file_editor.py
@@ -3,9 +3,9 @@ a_file = open("sample.txt", "r")
 list_of_lines = a_file.readlines()
 
 list_of_lines[1] = "Line2\n"
+new_filename = list_of_lines[0] + '.txt'
 
-
-a_file = open("sample.txt", "w")
+a_file = open(new_filename, "w")
 
 a_file.writelines(list_of_lines)
 
