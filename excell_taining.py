00d5f83b2fcb1529115b5a32fb8a44680761dfb2
Ulumanshu <karolis_kesminas@yahoo.com>
2021-10-30 12:09:27 +0300

    Add isinstance example

--- excell_taining.py
+++ excell_taining.py
@@ -19,19 +19,26 @@ def get_xlsx_sheet_data(xlsx_file_path):
 
 
 if __name__ == "__main__":
-    xlsx_file = '/home/wooden/workspace/sdacademy/group_8_remote/my_file.xlsx'
-    excell_data = get_xlsx_sheet_data(xlsx_file)
-    pprint(excell_data, indent=4)
+    # xlsx_file = '/home/wooden/workspace/sdacademy/group_8_remote/my_file.xlsx'
+    # excell_data = get_xlsx_sheet_data(xlsx_file)
+    # pprint(excell_data, indent=4)
+    #
+    # # workbook = Workbook('result.xlsx')
+    # with Workbook('result.xlsx') as workbook:
+    #     worksheet = workbook.add_worksheet(str('Sheet1'))
+    #     for row, data_line in enumerate(excell_data):
+    #         for col, data_value in enumerate(data_line):
+    #             if isinstance(data_value, datetime):
+    #                 data_value = data_value.strftime("%Y - %m - %d")
+    #             if isinstance(data_value, float):
+    #                 data_value = round(data_value, 2)
+    #
+    #             worksheet.write(row, col, data_value)  # Writes
+    #
+    # # workbook.close()
 
-    workbook = Workbook('result.xlsx')
-    worksheet = workbook.add_worksheet(str('Sheet1'))
-    for row, data_line in enumerate(excell_data):
-        for col, data_value in enumerate(data_line):
-            if isinstance(data_value, datetime):
-                data_value = data_value.strftime("%Y - %m - %d")
-            if isinstance(data_value, float):
-                data_value = round(data_value, 2)
-
-            worksheet.write(row, col, data_value)  # Writes
-
-    workbook.close()
+    varied_array = [1, 2.99, 'text', (1, 9), [1, 2], {'names': 200000}, {1, 2, 3}, None, True, False]
+    for e in varied_array:
+        print('ELEMENTAS', e)
+        print("ELEMENTO TIPAS", type(e))
+        print("ISINSTACE FLOAT/INT?: ", isinstance(e, (int, float)))
