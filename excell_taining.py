ab26791d00d3486142e222ebbe21f67acdd4d462
Ulumanshu <karolis_kesminas@yahoo.com>
2021-10-30 10:17:16 +0300

    Add writing part

--- excell_taining.py
+++ excell_taining.py
@@ -1,12 +1,14 @@
 import openpyxl
-import xlsxwriter
+# import xlsxwriter
+from xlsxwriter.workbook import Workbook
+from pprint import pprint
 
 
 def get_xlsx_sheet_data(xlsx_file_path):
     wb_obj = openpyxl.load_workbook(xlsx_file_path)
     sheet = wb_obj.active
     res = []
-    for row_ in sheet.iter_rows(max_col=10, min_row=1, max_row=1000):
+    for row_ in sheet.iter_rows():
         row_values = []
         for cell in row_:
             row_values.append(cell.value)
@@ -19,4 +21,12 @@ def get_xlsx_sheet_data(xlsx_file_path):
 if __name__ == "__main__":
     xlsx_file = '/home/wooden/workspace/sdacademy/group_8_remote/my_file.xlsx'
     excell_data = get_xlsx_sheet_data(xlsx_file)
-    print(excell_data)
+    pprint(excell_data, indent=4)
+
+    workbook = Workbook('result.xlsx')
+    worksheet = workbook.add_worksheet(str('Sheet1'))
+    for row, data_line in enumerate(excell_data):
+        for col, data_value in enumerate(data_line):
+            worksheet.write(row, col, data_value)  # Writes None
+
+    workbook.close()
