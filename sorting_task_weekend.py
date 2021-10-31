20fab350574dd353d68284c01a304a8f158cea79
Ulumanshu <karolis_kesminas@yahoo.com>
2021-10-31 13:53:17 +0200

    Add sorting task

---
+++ sorting_task_weekend.py
@@ -0,0 +1,51 @@
+
+
+big_list = [
+    {
+        "name": 'Abcd',
+        'age': 15,
+        'speed': 100,
+    },
+    {
+        "name": 'Fxccc',
+        'age': 15,
+        'speed': 12,
+    },
+    {
+        "name": 'Rgb',
+        'age': 453,
+        'speed': 1011,
+    },
+    {
+        "name": 'Avgregd',
+        'age': 7,
+        'speed': 11,
+    },
+    {
+        "name": 'Abuugcfcd',
+        'age': 3,
+        'speed': 100000,
+    },
+    {
+        "name": 'Abbrbreth',
+        'age': 15,
+        'speed': 100000,
+    },
+    {
+        "name": 'Abbrbertrhnrt',
+        'age': 15,
+        'speed': 10,
+    },
+    {
+        "name": 'Rbrbertrhnrt',
+        'age': 453,
+        'speed': 10,
+    },
+]
+# Uzdavinys:
+    # Surusiuoti sarasa trim budais:
+    # 1. Pagal name pirma raide A virsuj, amzius nuo didelio iki mazo, greitis nuo mazo iki didelio.
+    # 2. Amzius nuo didelio iki mazo, greitis nuo mazo iki didelio pagal abecele atvirkstine tvarka.
+    # 2. Greitis nuo mazo iki didelio, Amzius nuo mazo iki didelio, pagal abecele atvirkstine tvarka.
+
+    # rezultatus atspausdinti. su print()
