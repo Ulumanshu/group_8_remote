ba9e7b499a6df545b3d57472987eb842cb0de496
Ulumanshu <karolis_kesminas@yahoo.com>
2021-10-31 13:41:17 +0200

    Add lambda example in task more ...

--- lambda_example.py
+++ lambda_example.py
@@ -27,3 +27,9 @@ print(odds)
 print(odds_filter_comprehension)
 
 print(items_sum)
+
+pairs = [(1, 10), (2, 9), (3, 8)]
+sorted(pairs, key=lambda x: x[1])  # [(3, 8), (2, 9), (1, 10)]
+min(pairs)  # (1, 10)
+max(pairs, key=lambda x: x[1])  # (1, 10)
+max(pairs, key=lambda x: x[0] * x[1])  # (3, 8)
