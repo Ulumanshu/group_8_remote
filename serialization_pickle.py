028df67672730e9a2b3a88b7fe91743fa5395380
Ulumanshu <karolis_kesminas@yahoo.com>
2021-10-31 09:13:30 +0200

    Add pickle example

---
+++ serialization_pickle.py
@@ -0,0 +1,17 @@
+import pickle
+
+data = {
+    'a': [1, 2.0, 3, 4 + 6j],
+    'b': ("Alice has a cat", "Python programming is great"),
+    'c': [False, True, False]
+}
+
+# # Saving to pickle
+# with open('data.pickle', 'wb') as f:
+#     pickle.dump(data, f)
+
+
+# Loading from pickle
+with open('data.pickle', 'rb') as f:
+    data = pickle.load(f)
+print(data)
