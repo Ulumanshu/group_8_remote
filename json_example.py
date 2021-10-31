d1080bbfb3454b2710c1f2784936060f0d0561c2
Marius <marius.jukna7@gmail.com>
2021-10-31 12:00:08 +0200

    Add html parser example

--- json_example.py
+++ json_example.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     with open("pets.json") as in_file:
         data = json.load(in_file)
 
-    print(data['pets'])
+    # print(data['pets'])
     all_pets = data['pets']
 
     # Uzduotis:
@@ -15,19 +15,30 @@ if __name__ == "__main__":
     # ir irasyti i skirtingus json failus
     cats_dict = dict()  # dict() - {}, list() - [], tuple() - () dict(), list(), tuple(1, 2, )
     dogs_dict = dict()
-    split_keys = [('Cat', cats_dict), ('Dog', dogs_dict)]
-    for split_key in split_keys:
-        species_name = split_key[0]
-        species_dictionary = split_key[1]
-        print(split_key, species_name, species_dictionary)
-        for pet in all_pets:
-            pet_species = pet.get('species', '') or ''
-            pet_name = pet.get('name', '') or ''
-            if pet_species == species_name:
-                species_dictionary[pet_name] = pet
-        print(split_key, species_name, species_dictionary)
+
+    cats_dict['pets'] = []
+    dogs_dict['pets'] = []
+
+    for pet in all_pets:
+        # print(pet)
+        # print(type(pet))
+        pet_species = pet.get('species')
+        # print(pet_species)
+        pet_name = pet.get('name')
+        # print(pet_species, pet_name)
+        if pet_species == 'Dog':
+            # dogs_dict.update({pet_name: pet})  # .update(pet_name=pet)
+            dogs_dict['pets'].append(pet)
+        elif pet_species == 'Cat':
+            # cats_dict[pet_name] = pet
+            cats_dict['pets'].append(pet)
+    print(dogs_dict)
+    print(cats_dict)
+
+    with open("dogs.json", 'w') as out_file:
+        json.dump(dogs_dict, out_file, indent=4)
+
+    with open("cats.json", 'w') as out_file:
+        json.dump(cats_dict, out_file, indent=4)
 
 
-        # Iraso pitono objekta i json faila, suformatuoja objekta (kabutes, atstumai, etc.) kad atitiktu json formata
-        with open(f"{species_name}.json", 'w') as out_file:
-            json.dump(species_dictionary, out_file, indent=4)
