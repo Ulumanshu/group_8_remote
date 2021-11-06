9cbaa88eb7fae088447eb4556cfbae56a0ac59dc
Ulumanshu <karolis_kesminas@yahoo.com>
2021-11-06 13:44:38 +0200

    Add generators expl

--- generators_iterators.py
+++ generators_iterators.py
@@ -21,8 +21,29 @@ def prime_generator(n):
         number += 1
 
 
+def prime_former(n):
+    # form list of primes
+    res = []
+    number = 2
+    generated_numbers = 0
+    while generated_numbers != n:
+        if is_prime(number):
+            print("Look in Former Happens!!!")
+            res.append(number)
+
+            generated_numbers += 1
+        number += 1
+
+    return res
+
 gen = prime_generator(10)
 print(type(gen))
 for elem in gen:
     a = input("Continue: ")
     print(elem)
+
+formed = prime_former(10)
+for elem_f in formed:
+    a = input("Continue: ")
+    print(elem_f)
+
