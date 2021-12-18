08010e5abb48913e6470d203e69905343ba1dbc3
Ulumanshu <karolis_kesminas@yahoo.com>
2021-12-18 13:02:23 +0200

    Add minitas solution step 1

--- multiprocessing_minitasks.py
+++ multiprocessing_minitasks.py
@@ -1,37 +1,55 @@
 import timeit
 import requests
 
+URLS = [
+    "https://jsonplaceholder.typicode.com/comments/1",
+    "https://jsonplaceholder.typicode.com/comments/2",
+    "https://jsonplaceholder.typicode.com/comments/3"
+]
 
-def crawl(url, dest):
+
+def crawl(url):
+    res = []
     try:
         result = requests.get(url).text
-        with open(dest, "a") as f:
-            f.write(result)
+        res.append(result)
 
     except requests.exceptions.RequestException:
         print("Error with URL check!")
 
+    return res
+
 
 def wo_threading_func(urls):
+    res = []
     for url in urls:
-        crawl(url, "without_threads.txt")
+        subres = crawl(url)
+        res.extend(subres)
+
+    return res
 
 
 def with_threading_func(urls):
     import threading
-
+    res = []
     threads = []
     for url in urls:
-        th = threading.Thread(target=crawl, args=(url, "with_threads.txt"))
+        th = threading.Thread(target=crawl, args=(url, ))
         th.start()
         threads.append(th)
 
     for th in threads:
-        th.join()
+        res.extend(th.join() or "NONE")
+
+    return res
 
 
 if __name__ == "__main__":
-    pass
+    crawl_no_thread_results = wo_threading_func(URLS)
+    crawl_with_threads_results = with_threading_func(URLS)
+
+    print(crawl_no_thread_results)
+    print(crawl_with_threads_results)
     # Pamodifikuoti metodus with_threading_func ir wo_threading_func bei crawl taip,
     # kad jie grazintu ka nuskaite internete kokia nors forma, ir su tredais ir be
     # (threading clase join metodo paveldejimas)
